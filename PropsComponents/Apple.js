8a9fc65341c2dc7da831fba1aa0a88615571fb38
Mahesh Yadav <ymahesh756@gmail.com>
2023-09-22 13:51:09 +0530

    c5

--- PropsComponents/Apple.js
+++ PropsComponents/Apple.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import B from "./B"
+import React from "react";
+import B from "./B";
+import Z from "./Z";
 
 const Apple = (p) => {
   return (
     <div>
-    <>
+      <>
         <h1>
           Apple component {p.name} {p.msg}
         </h1>
         <B name="Mayank" />
+        <Z name="Anil" msg="Oklabs" />
       </>
     </div>
   );
-}
+};
 
-export default Apple
+export default Apple;
